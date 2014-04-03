79c6a52d4fe0531cd6f12dfc36923ef91d3fb145
shidel.dev <joeshidel@gmail.com>
2014-04-03 08:20:35 -0500

    improvement on original solution

--- euler_two.clj
+++ euler_two.clj
@@ -1,15 +1,10 @@
-(ns euler)
+ (ns euler)
 
-(defn even_fibs [limit]
-  (loop [prev 0 cur 1 sum 0]
-    (def new_cur (+' cur prev))
-    (println new_cur)
-      (if (< new_cur limit)
-        (if (zero? (mod new_cur 2))
-          (recur cur new_cur (+ sum new_cur))
-        (recur cur new_cur sum))
-      sum)
-  )
-)
+ (defn even_fibs [limit]
+   (loop [prev 0 cur 1 sum 0]
+     (if (< prev limit)
+       (recur cur (+' cur prev) (if (zero? (mod cur 2)) (+' sum cur) sum))
+      sum)))
 
-(even_fibs 100)
+ (even_fibs 4000000)
+
